e0f6a55ab245669c84935ec0422a4228dbdf2191
yousefmatar99 <yousef.matar.99@gmail.com>
2025-04-12 16:48:05 +0300

    added data models (customer, partner, ...), added dataService, added customer component as an example of how to combine the dataService with customer component with customer mocdel (customer class)

---
+++ src/app/services/data.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, map } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+
+import { Customer } from '../models/customer.model';
+import { Partner } from '../models/partner.model';
+import { Reservation } from '../models/reservation.model';
+import { Package } from '../models/package.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+
+  baseUrl: string = "https://api.wosh.co.il/api/administrator"
+
+  // Behavioral Subjects
+  private customersSubject = new BehaviorSubject<Customer[]>([]);
+  private partnersSubject = new BehaviorSubject<Partner[]>([]);
+  private reservationsSubject = new BehaviorSubject<Reservation[]>([]);
+  private packagesSubject = new BehaviorSubject<Package[]>([]);
+
+  constructor(private http: HttpClient) { }
+
+  fetchCustomers(): void {
+    this.http.get<any[]>(`${this.baseUrl}/customers`)
+      .pipe(map(data => data.map(item => Customer.fromJson(item))))
+      .subscribe(customers => this.customersSubject.next(customers));
+  }
+
+}
